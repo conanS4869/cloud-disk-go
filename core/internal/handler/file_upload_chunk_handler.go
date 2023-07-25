350216daa93cabd4fe15d5ee420894d3c0d8ddb8
conanS4869 <87404816@qq.com>
2023-07-25 13:57:34 +0800

    1

--- core/internal/handler/file_upload_chunk_handler.go
+++ core/internal/handler/file_upload_chunk_handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"cloud-disk-go/core/helper"
+	"errors"
 	"net/http"
 
 	"cloud-disk-go/core/internal/logic"
@@ -16,9 +18,28 @@ func FileUploadChunkHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
 			httpx.ErrorCtx(r.Context(), w, err)
 			return
 		}
+		// 参加必填校验
+		if r.PostForm.Get("key") == "" {
+			httpx.Error(w, errors.New("key is empty"))
+		}
+		if r.PostForm.Get("upload_id") == "" {
+			httpx.Error(w, errors.New("upload_id is empty"))
+		}
+		if r.PostForm.Get("part_number") == "" {
+			httpx.Error(w, errors.New("part_number is empty"))
+		}
+
+		etag, err := helper.CosPartUpload(r)
+		if err != nil {
+			httpx.Error(w, err)
+			return
+		}
 
 		l := logic.NewFileUploadChunkLogic(r.Context(), svcCtx)
 		resp, err := l.FileUploadChunk(&req)
+		resp = new(types.FileUploadChunkResponse)
+		resp.Etag = etag
+
 		if err != nil {
 			httpx.ErrorCtx(r.Context(), w, err)
 		} else {
